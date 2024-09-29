41dd5e39c25fdd90b5ccd0844a783236cb0b65a7
abanoub maged <abanoubmagedmaher@gmail.com>
2024-09-29 20:36:20 +0300

    Hellow Dev Team :)

--- ATechSystem/Program.cs
+++ ATechSystem/Program.cs
@@ -7,6 +7,7 @@ namespace ATechSystem
         {
             var builder = WebApplication.CreateBuilder(args);
 
+
             // Add services to the container.
 
             builder.Services.AddControllers();
